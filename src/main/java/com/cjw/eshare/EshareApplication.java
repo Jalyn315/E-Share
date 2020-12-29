c9a51fa30a0699f3ed84702fe44b0465dace026b
Jalyn <1921776332@qq.com>
2020-12-30 00:09:57 +0800

    create project

---
+++ src/main/java/com/cjw/eshare/EshareApplication.java
@@ -0,0 +1,13 @@
+package com.cjw.eshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshareApplication.class, args);
+	}
+
+}
