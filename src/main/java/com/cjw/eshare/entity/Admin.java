2f3aa0cd9a7caa3203d2da06035c0d1833c2067e
Jalyn <1921776332@qq.com>
2021-03-07 15:31:23 +0800

    分别对User和admin进行扩展，实现UserDetail接口

--- src/main/java/com/cjw/eshare/entity/Admin.java
+++ src/main/java/com/cjw/eshare/entity/Admin.java
@@ -1,8 +1,11 @@
 package com.cjw.eshare.entity;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.Date;
 
 /**
@@ -15,7 +18,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class Admin implements Serializable {
+public class Admin implements Serializable, UserDetails {
     private Integer id;
     private String admin_user;
     private String admin_pass;
@@ -23,4 +26,40 @@ public class Admin implements Serializable {
     private Date login_at;
     private String login_ip;
     private Date create_at;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return admin_pass;
+    }
+
+
+    @Override
+    public String getUsername() {
+        return admin_user;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/cjw/eshare/entity/User.java
+++ src/main/java/com/cjw/eshare/entity/User.java
@@ -1,8 +1,11 @@
 package com.cjw.eshare.entity;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.Date;
 
 /**
@@ -14,7 +17,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
     private Integer id;
     private String username;
     private String avatar_url;
@@ -24,4 +27,40 @@ public class User implements Serializable {
     private Date create_at;
     private Date update_at;
     private Date login_at;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
 }
