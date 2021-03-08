ad90ccc9ac5a1a2143b0732c49f35dc318cf68f8
Jalyn <1921776332@qq.com>
2021-03-08 14:21:56 +0800

    完成用户操作

--- src/main/java/com/cjw/eshare/controller/UserController.java
+++ src/main/java/com/cjw/eshare/controller/UserController.java
@@ -1,19 +1,69 @@
 package com.cjw.eshare.controller;
 
+import com.cjw.eshare.entity.User;
+import com.cjw.eshare.entity.UserExtend;
+import com.cjw.eshare.model.CRModel;
+import com.cjw.eshare.model.LoginModel;
+import com.cjw.eshare.model.RegisterModel;
+import com.cjw.eshare.service.impl.UserService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.ConditionalOnRepositoryType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author cj.w
  * @date 2021/1/6 23:26
  */
-@Controller
+@RestController
 public class UserController {
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String sayHello(){
-        return "hello";
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 注册
+     * @param model，用户输入的信息
+     * @param request
+     * @return
+     */
+    @ApiOperation("注册")
+    @PostMapping("/register")
+    public CRModel register(@RequestBody RegisterModel model, HttpServletRequest request) {
+        return userService.registration(model, request);
     }
+
+
+    @ApiOperation(value = "登陆")
+    @PostMapping("/login")
+    public CRModel login(@RequestBody LoginModel model, HttpServletRequest request) {
+        return userService.login(model, request);
+    }
+
+
+    @ApiOperation(value = "修改密码")
+    @PostMapping("/change_password")
+    public CRModel changePassword(String oldPassword, String newPassword) {
+        return userService.changePassword(oldPassword, newPassword);
+    }
+
+    @ApiOperation(value = "获取用户信息")
+    @GetMapping("/user_info")
+    public CRModel getUserInfo() {
+        return userService.getUserInfo();
+    }
+
+    @ApiOperation(value = "更改用户个人信息")
+    @PostMapping("/update_user_info")
+    public CRModel updateUserInfo(@RequestBody UserExtend userInfo) {
+        return userService.updateUserInfo(userInfo);
+    }
+
+
 }
