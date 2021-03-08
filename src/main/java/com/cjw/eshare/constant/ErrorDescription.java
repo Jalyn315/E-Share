d59caf6cb4881a864fa9b35f7b5509998173d784
Jalyn <1921776332@qq.com>
2021-03-08 14:21:31 +0800

    新增错误OT常量类和成功OT常量类

---
+++ src/main/java/com/cjw/eshare/constant/ErrorDescription.java
@@ -0,0 +1,15 @@
+package com.cjw.eshare.constant;
+
+/**
+ * @author cj.w
+ * @program: eshare
+ * @description: 错误描述
+ * @create: 2021/3/7 15:56
+ */
+public class ErrorDescription {
+
+    public static final String CAPTCHA_ERR = "验证码输入有误，请从新输入!";
+    public static final String USERNAME_PWD_ERR = "用户名或者密码有误，请重新输入！";
+    public static final String CHANGE_PWD_ERR = "修改密码失败，您输入的原密码有误！";
+    public static final String CHANGE_PWD_ERR1 = "修改密码失败，请稍后在重试！";
+}
---
+++ src/main/java/com/cjw/eshare/constant/SuccessDescription.java
@@ -0,0 +1,14 @@
+package com.cjw.eshare.constant;
+
+/**
+ * @author cj.w
+ * @program: eshare
+ * @description: 请求响应成功描述
+ * @create: 2021/3/7 16:11
+ */
+public class SuccessDescription {
+
+    public static final String LOGIN_SUCCESS = "登陆成功!";
+    public static final String CHANGE_PWD_SUCCESS = "密码修改成功，请重新登陆!";
+    public static final String UPDATE_USER_INFO_SUCCESS = "更新用户信息成功！";
+}
